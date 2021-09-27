8a092ee9802cbba204c494eba85e712b1a0fcc3a
Kasper Olsen <kasper@kols.dk>
2021-09-27 21:31:33 +0200

    fixed buttons

--- src/components/Hyperlink.vue
+++ src/components/Hyperlink.vue
@@ -32,12 +32,11 @@ export default {
 </script>
 
 <style scoped>
-/* https://codepen.io/leandrosimoes/pen/VqZxaG */
 .button {
   border: none;
   outline: none;
   color: #fff;
-  background: hsl(var(--brand-primary));
+  background: hsl(var(--brand-secondary));
   cursor: pointer;
   position: relative;
   z-index: 0;
@@ -51,73 +50,30 @@ export default {
   text-decoration: none;
 }
 
-.button::before:active {
-  background: linear-gradient(
-    45deg,
-    #ff0000,
-    #ff7300,
-    #fffb00,
-    #48ff00,
-    #00ffd5,
-    #002bff,
-    #7a00ff,
-    #ff00c8,
-    #ff0000
+/* https://codepen.io/althi/pen/eKdmaa */
+.button:hover {
+  background-image: linear-gradient(
+    to right,
+    #e7484f,
+    #e7484f 16.65%,
+    #f68b1d 16.65%,
+    #f68b1d 33.3%,
+    #fced00 33.3%,
+    #fced00 49.95%,
+    #009e4f 49.95%,
+    #009e4f 66.6%,
+    #00aac3 66.6%,
+    #00aac3 83.25%,
+    #732982 83.25%,
+    #732982 100%,
+    #e7484f 100%
   );
+  animation: slidebg 2s linear infinite;
 }
 
-.button:before {
-  content: "";
-  background: linear-gradient(
-    45deg,
-    #ff0000,
-    #ff7300,
-    #fffb00,
-    #48ff00,
-    #00ffd5,
-    #002bff,
-    #7a00ff,
-    #ff00c8,
-    #ff0000
-  );
-  position: absolute;
-  top: -2px;
-  left: -2px;
-  background-size: 400%;
-  z-index: -1;
-  filter: blur(5px);
-  width: calc(100% + 4px);
-  height: calc(100% + 4px);
-  animation: glowing 20s linear infinite;
-  opacity: 0;
-  transition: opacity 0.2s ease-in-out;
-}
-
-.button:hover:before,
-.button:focus:before {
-  opacity: 1;
-}
-
-.button:after {
-  z-index: -1;
-  content: "";
-  position: absolute;
-  width: 100%;
-  height: 100%;
-  background: hsl(var(--brand-secondary));
-  left: 0;
-  top: 0;
-}
-
-@keyframes glowing {
-  0% {
-    background-position: 0 0;
-  }
-  50% {
-    background-position: 400% 0;
-  }
-  100% {
-    background-position: 0 0;
+@keyframes slidebg {
+  to {
+    background-position: 20vw;
   }
 }
 </style>
