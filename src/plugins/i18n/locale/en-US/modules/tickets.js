1ca8452660a2cb2097ccf8850cda168885e90b1c
Kasper Olsen <kasper@kols.dk>
2022-03-15 22:26:11 +0100

    updated texts

--- src/plugins/i18n/locale/en-US/modules/tickets.js
+++ src/plugins/i18n/locale/en-US/modules/tickets.js
@@ -1,22 +1,20 @@
 export default {
-    title: "Buy Tickets",
-    steps: {
-        findSeat: {
-            title: "Find seat",
-            stepNumber: "1.",
-            text:
-                "Choose your table and seat number during the booking process. The table number from your booking is final. Ignore any other table numbers than the ones posted on ITU LAN posters on the tables.",
-        },
-        buyTicket: {
-            title: "Buy Tickets",
-            stepNumber: "2.",
-            text: "Buy your tickets here",
-        },
-        checkin: {
-            title: "Check-in",
-            stepNumber: "3.",
-            text:
-                "When you arrive, please check-in at 4A10. Here you will also receive your exclusive ITU LAN wristband.",
-        },
+  title: "Buy Tickets",
+  steps: {
+    findSeat: {
+      title: "Find seat",
+      stepNumber: "1.",
+      text: "Choose your table and seat number during the booking process. The table number from your booking is final. Ignore any other table numbers than the ones posted on ITU LAN posters on the tables.",
     },
-}
+    buyTicket: {
+      title: "Buy Tickets",
+      stepNumber: "2.",
+      text: "Buy your tickets here",
+    },
+    checkin: {
+      title: "Check-in",
+      stepNumber: "3.",
+      text: "When you arrive, please check-in on the 1st floor. Here you will also receive your exclusive ITU LAN wristband.",
+    },
+  },
+};
