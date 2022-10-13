6984af2a3d17dfd02473318a226c9a7acb7af08e
Marius <Mariu@itu.dk>
2022-10-13 17:38:03 +0100

    Tournaments published

--- src/plugins/i18n/locale/en-US/modules/tournaments.js
+++ src/plugins/i18n/locale/en-US/modules/tournaments.js
@@ -10,17 +10,26 @@ export default {
     platformLabel: "Platform: ",
     tournaments: [
       {
-        title: "Randourment",
-        widgetUrl:
-          "https://widget.toornament.com/tournaments/5474694281440600064/?_locale=en_US",
-        platform: "PC",
-        description: "Prepare yourself for our next customized expereience - Randourment!! A tournament where you not only get to test out your skills in one diciplin, but several diciplins ranging from gaming to non-gaming contests! The way Randourment works is that the diciplins you will compete in is going to be revealed throughout the friday, where you guys have time to download and prepare the games for saturdays games. During saturday we will go throuh each diciplin where you guys can prove your worth in each of them. Wanna show that you are super duper awesome? then join the Randourment and make your opponents seems like noobs! And proove you can be the best at beeing random!",
-        logoPath: "",
+      title: "League of legends",
+      widgetUrl: 
+      " https://widget.toornament.com/tournaments/6081254709717803008/ ",
+      platform: " PC ",
+      description:"We're finally back with a League of Legends tournament for the ITU LAN F'22! Gather your 5 player team, and get ready to bask in the glory reflected in the salty tears of your opponents. The tournament will be open for up to 12 teams, which must be present physically at ITU LAN F'22 with valid tickets. First stage of the tournament is scheduled to begin at 14:00 / 2PM on Saturday, November 5th. Check-in at 13:00 / 1PM, after which we settle on the final tournament structure, based on the amount of teams present. Registration is on a first-come-first-served principle. - Sign up early to secure your spot! - Remember to read the rules if you sign up! https://play.toornament.com/en_US/tournaments/6081254709717803008/rules ",
+      logoPath: ""
+      },
+
+      {
+      title: " Guilty gear",
+      widgetUrl: 
+      "https://widget.toornament.com/tournaments/6110547244282544128/",
+      platform: "Console",
+      description:" ",
+      logoPath: "",
       },
       {
         title: "Mario Kart 8 Deluxe",
         widgetUrl:
-          "https://widget.toornament.com/tournaments/5472694329367420928/?_locale=en_US",
+          "https://widget.toornament.com/tournaments/6108319740569935872/",
         platform: "Nintendo Switch",
         description: "Once again, we start our engines for another Mario Kart tournament! The game is the same as last time: Mario Kart 8 Deluxe, random stage cups, all items! Come along, blue shell your way to the top and claim victory! (Disclaimer: We cannot guarantee that your victory will not cost a friendship or two, this is Mario Kart, after all)",
         logoPath: "",
@@ -28,23 +37,23 @@ export default {
       {
         title: "Super Smash Bros",
         widgetUrl:
-          "https://widget.toornament.com/tournaments/5472690415041945600/?_locale=en_US",
+          "https://widget.toornament.com/tournaments/6112106790036668416/",
         platform: "Nintendo Switch",
-        description: "Super Smash Bros are returning for this years LAN, and again we're hosting a tournament!",
+        description: "Super Smash Bros are returning for this years LAN, and again we're hosting a tournament! Smash Bros will happen 5th November 14:00, at the Console Corner (1. floor) (Please note: Gamecube adapters and controllers will not be supplied; if you want to use that for the tournament, please bring your own",
         logoPath: "",
       },
       {
-        title: "Tekken 7",
+        title: "Splatoon",
         widgetUrl:
-          "https://widget.toornament.com/tournaments/5472666588207128576/?_locale=en_US",
-        platform: "PC",
-        description: "ITU LAN is calling fighters of all levels for the Tekken 7 tournament! Will you rise to the challenge in this 1v1 tournament and claim the title of champion? on friday the 8th, at 20:00 we will find our champion!",
+          "https://widget.toornament.com/tournaments/6112139226468483072/?_locale=en_US",
+        platform: "Splatoon",
+        description: "Welcome to the ITULAN Splatfest! In this colorful tournament there will be ink shed in this competitive squid game. The tournament will start the 5th November, 16:00 at Console Corner (1st Floor).",
         logoPath: "",
       },
       {
         title: "Europa Universalis IV",
         widgetUrl:
-          "https://widget.toornament.com/tournaments/5472565344494919680/?_locale=en_US",
+          "https://widget.toornament.com/tournaments/6092791349581209600/",
         platform: "PC",
         description: "Are you looking to scratch your strategy itch? Well then luckily for you, ITU LAN is hosting an EU4 game as part of this semesters event. The game will be played over the course of two 4-hour sessions: one on Friday, and one on Saturday. Both session last from 7pm until 11pm. Countries are handed out on a first come, first served-basis, and you can play as any European or Mediterranean country below 150 development. Alliances are allowed, so feel free to bring a friend, as is multiple people playing as one country. Just try to keep things interesting.",
         logoPath: "",
@@ -52,7 +61,7 @@ export default {
       {
         title: "Just Dance 2022",
         widgetUrl:
-          "https://widget.toornament.com/tournaments/5472537729029554176/?_locale=en_US",
+          " https://widget.toornament.com/tournaments/6080800903633051648/?_locale=en_US",
         platform: "Nintendo Switch",
         description: "Do you like to dance? Do you like to have fun?? Do you like to win awesome prizes??? If you can answer \"yes\" to all the questions above, then you need to join ITU LAN's JUST DANCE 2022 TOURNAMENT! You don't need any previous dance experience. Come as you are :-)",
         logoPath: "",
@@ -60,7 +69,7 @@ export default {
       {
         title: "Rocket League 2v2",
         widgetUrl:
-          "https://widget.toornament.com/tournaments/5472520437347909632/?_locale=en_US",
+          "https://widget.toornament.com/tournaments/6081220856220180480/?_locale=en_US",
         platform: "PC",
         description: "Once again we are proud to announce the Rocket League Tournament for ITU LAN Spring'22. Just as the previous tournaments, it will have the format of 2v2 Soccar on PC. Choose your partner wisely and demolition, freestyle, and pinch-ball your way to victory.",
         logoPath: "",
@@ -68,17 +77,38 @@ export default {
       {
         title: "Escape Room",
         platform: "IRL",
-        description: "Escape Room will return for ITU LAN Spring 22! Invite your friends (4-6 people per group) for the heist of your lives! A prestigious private collection is being shown off at ITU, but a tip has informed you that by the end of night everything will be sold off. Can you and your team steal, get away with and save the most beautiful painting of them all? Sign up <a target=\"_blank\" href='https://doodle.com/meeting/participate/id/NbWZ0Ega/vote'>here</a> ASAP - few spots available!",
+        description: " - not Done",
         logoPath: "",
       },
       {
         title: "Counter Strike: Global Offensive",
         widgetUrl:
-          "https://widget.toornament.com/tournaments/5565166619507105792/?_locale=en_US",
+          "https://widget.toornament.com/tournaments/6081254709717803008/?_locale=en_US",
         platform: "PC",
         description: "The CS:GO tournament is the best way for you and your team to compete against other dedicated teams. You will have the time of your life beating those n00bs on the other teams. So gather four of your friends and sign up for the ultimate intense heartbreaking mindblowing competition, the ITU LAN CS:GO tournament! #GottaShootEmAll",
         logoPath: "",
       },
+      {
+        title: "Valorant",
+        widgetUrl:
+          "https://widget.toornament.com/tournaments/6093003091998597120/?_locale=en_US",
+        platform: "PC",
+        description: "The Valorant tournament is gonna be a single elimination tournament. If you do not have a full team/are participating solo, feel free to message in the 'Lookingforgroup' channel on the official ITU LAN Discord server. Alternatively, you can message me directly on Discord (TriaTheSpartanGR#9320) so I can help you find teammates!",
+        logoPath: "",
+      },
+      {
+        title: "Magic the gathering ",
+        widgetUrl: "https://widget.toornament.com/tournaments/",
+        platform: " Table top",
+        logoPath: "",
+      },
+      // {
+      //   title: "",
+      //   widgetUrl: "",
+      //   platform: " ",
+      //   logoPath: "",
+      // },
+
     ],
   },
 }
--- src/plugins/router.js
+++ src/plugins/router.js
@@ -2,10 +2,10 @@ import Vue from "vue";
 import Router from "vue-router";
 import Home from "@/views/Home.vue";
 // import News from "@/views/News.vue";
-// import Tournaments from "@/views/Tournaments.vue";
+import Tournaments from "@/views/Tournaments.vue";
 import Information from "@/views/Information.vue";
 import Tickets from "@/views/Tickets.vue";
-import JoinUs from "@/views/JoinUs.vue";
+//import JoinUs from "@/views/JoinUs.vue";
 import Partner from "@/views/Partner.vue";
 import NotFound from "@/views/NotFound.vue";
 // import Schedule from "@/views/Schedule.vue";
@@ -53,15 +53,15 @@ const router = new Router({
     //     id: "news"
     //   }
     // },
-    // {
-    //   path: "/tournaments",
-    //   name: "tournaments",
-    //   component: Tournaments,
-    //   meta: {
-    //     title: "Tournaments",
-    //     id: "tournaments"
-    //   }
-    // },
+    {
+      path: "/tournaments",
+      name: "tournaments",
+      component: Tournaments,
+      meta: {
+        title: "Tournaments",
+        id: "tournaments"
+      }
+    },
     {
       path: "/information",
       name: "faq",
@@ -71,15 +71,15 @@ const router = new Router({
         id: "information"
       }
     },
-    {
-      path: "/join",
-      name: "join the crew",
-      component: JoinUs,
-      meta: {
-        title: "Join the Crew",
-        id: "join"
-      }
-    },
+    //{
+    //   path: "/join",
+    //   name: "join the crew",
+    //   component: JoinUs,
+    //   meta: {
+    //     title: "Join the Crew",
+    //     id: "join"
+    //   }
+    // },
     {
       path: "/partner",
       name: "partner with us",
