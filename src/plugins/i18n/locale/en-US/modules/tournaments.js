e11e6262be192cc74e33e1a0b6b54b752845644c
AlbinoLoaf <Martho2000@gmail.com>
2023-03-24 10:34:51 +0100

    Discription update

--- src/plugins/i18n/locale/en-US/modules/tournaments.js
+++ src/plugins/i18n/locale/en-US/modules/tournaments.js
@@ -14,24 +14,22 @@ export default {
       widgetUrl: 
       " https://widget.toornament.com/tournaments/6545134842597015552/ ",
       platform: " PC ",
-      description:"We're finally back with a League of Legends tournament for the ITU LAN F'22! Gather your 5 player team, and get ready to bask in the glory reflected in the salty tears of your opponents. The tournament will be open for up to 12 teams, which must be present physically at ITU LAN F'22 with valid tickets. First stage of the tournament is scheduled to begin at 15:00 / 4PM on Saturday, November 5th. Check-in at 14:00 / 2PM, after which we settle on the final tournament structure, based on the amount of teams present. Registration is on a first-come-first-served principle. - Sign up early to secure your spot! - Remember to read the rules if you sign up! https://play.toornament.com/en_US/tournaments/6081254709717803008/rules ",
+      description:"We're back with a League of Legends tournament for the ITU LAN S'23! Gather your 5 player team, and get ready to bask in the glory reflected in the salty tears of your opponents. This time we'll be using an external draft tool for champion selection (tournament draft): https://draftlol.dawe.gg/ The tournament will be open for up to 12 teams, which must be present physically at ITU LAN S'23 with valid tickets. First stage of the tournament is scheduled to begin at 12:00 on Saturday, April 1st. Check-in at 11:00 / 11 AM, after which we settle on the final tournament structure, based on the amount of teams present. Registration is on a first-come-first-served principle.- Sign up early to secure your spot! - Remember to read the rules if you sign up! https://play.toornament.com/en_GB/tournaments/6545134842597015552/rules The overall tournament has a potential duration of ~8 hours (depending on number of signups), but will include a scheduled recess (tba)",
       logoPath: ""
-      },
-
-      
+      }, 
       {
         title: "Mario Kart 8 Deluxe 17:00 Fri",
         widgetUrl:
           "https://widget.toornament.com/tournaments/6527856000850731008/",
         platform: "Nintendo Switch",
-        description: "Once again, we start our engines for another Mario Kart tournament! The game is the same as last time: Mario Kart 8 Deluxe, random stage cups, all items! Come along, blue shell your way to the top and claim victory! (Disclaimer: We cannot guarantee that your victory will not cost a friendship or two, this is Mario Kart, after all)",
+        description: "Come along, blue shell your way to the top and claim victory! 31st of March at 17.00! at Console Corner (1st Floor). (Disclaimer: We cannot guarantee that your victory will not cost a friendship or two, this is Mario Kart, after all) Please join our Discord to keep track with the tournament as we can only have 4 people play at a time! But please stick around for snack and insane drifts. discord.com/invite/nSFXWQN",
         logoPath: "",
       },
       {
         title: "Super Smash Bros 20:00 Sat",
         widgetUrl:"https://widget.toornament.com/tournaments/6551353359086403584/",
         platform: "Nintendo Switch",
-        description: "Super Smash Bros are returning for this years LAN, and again we're hosting a tournament! Smash Bros will happen 5th November 14:00, at the Console Corner (1. floor) (Please note: Gamecube adapters and controllers will not be supplied; if you want to use that for the tournament, please bring your own",
+        description: "Super Smash Bros are returning for this years LAN, and again we're hosting a tournament! Smash Bros is planned for the evening Saturday (20:00) (Please note: Gamecube adapters and controllers will not be supplied; if you want to use that for the tournament, please bring your own)",
         logoPath: "",
       },
       {
@@ -39,7 +37,7 @@ export default {
         widgetUrl:
           "https://widget.toornament.com/tournaments/6545189448331485184/",
         platform: "PC",
-        description: "Are you looking to scratch your strategy itch? Well then luckily for you, ITU LAN is hosting an EU4 game as part of this semesters event. The game will be played over the course of two 4-hour sessions: one on Friday, and one on Saturday. Both session last from 7pm until 11pm. Countries are handed out on a first come, first served-basis, and you can play as any European or Mediterranean country below 150 development. Alliances are allowed, so feel free to bring a friend, as is multiple people playing as one country. Just try to keep things interesting.",
+        description: "Are you a fan of strategy games? Do you enjoy making fragile alliances with other players, only to betray them when they are most vulnerable? And most importantly, do you like watching big numbers get bigger? Then you're going to love the Victoria 3 tournament at this year's ITU LAN. The game will be played in two parts, from 19:00 to 23:00 on both Friday and Saturday. Countries are handed out on a first come, first serve-basis with all great and major powers (beside Qing and the EIC) being eligible. We will also be playing with a couple of UI mods (because they're nice): https://steamcommunity.com/sharedfiles/filedetails/?id=2947703253",
         logoPath: "",
       },
       {
@@ -47,7 +45,7 @@ export default {
         widgetUrl:
           " https://widget.toornament.com/tournaments/6544576613659656192/",
         platform: "Nintendo Switch",
-        description: "Do you like to dance? Do you like to have fun?? Do you like to win awesome prizes??? If you can answer \"yes\" to all the questions above, then you need to join ITU LAN's JUST DANCE 2022 TOURNAMENT! You don't need any previous dance experience. Come as you are :-)",
+        description: "IT'S NOT AN APRIL FOOLS JOKE; JUST DANCE IT BACK, BABY! Do you like to dance? Do you like to have fun?? Do you like to win awesome prizes??? If you can answer YES to all the questions above, you must join ITU LAN's JUST DANCE 2023 TOURNAMENT! You don't need any previous dance experience. Come as you are :^) The tournament starts the 01/04/2023 @14:00 and takes place in Console Corner.",
         logoPath: "",
       },
       
@@ -62,14 +60,14 @@ export default {
         title: "Valorant 20.00  ",
         widgetUrl:"https://widget.toornament.com/tournaments/6568362849088102400/",
         platform: "PC",
-        description: "The Valorant tournament is gonna be a single elimination tournament. If you do not have a full team/are participating solo, feel free to message in the 'Lookingforgroup' channel on the official ITU LAN Discord server. Alternatively, you can message me directly on Discord (TriaTheSpartanGR#9320) so I can help you find teammates!",
+        description: "The Valorant tournament is going be a single elimination tournament. If you do not have a full team/are participating solo, feel free to message in the \"lookingforgroup\" channel on the official ITU LAN Discord server. Alternatively, you can message us directly on Discord (TriaTheSpartanGR#9320) so we can help you find teammates! discord.com/invite/nSFXWQN",
         logoPath: "",
       },
       {
         title: "Magic the gathering 17.00 Fri",
         widgetUrl: "https://widget.toornament.com/tournaments/6545164054309625856/",
         platform: " Table top",
-        description: " Draft starts Saturday 5th of Nov at 12:00 sharp, so please come in good time at least 10-15min before for attendance call. Players will be distributed into 4 groups of 6 players. The structure of the tournament will be double round robin within each pod, meaning a total of 10 matches per player. This is a PHANTOM Draft. Cards used to make your decks during the draft ARE NOT KEPT afterwards. Please return any card and/or basic lands you may have borrowed during the draft :) You may of course keep any prizes you earn in the tournament. We encourage you to bring your own basic lands and card sleeves ",
+        description: " We're going to hold a paper magic draft again this year. However, unlike last year we're going to drafting from 3 different sets, so semi chaos. Please show up 20min before the draft for role calls. Also bring your own sleeves and basic lands just for good measure :) Players are not to keep the drafted cards. However, winners are awarded booster packs as prizes.",
         logoPath: "",
       },
       {
